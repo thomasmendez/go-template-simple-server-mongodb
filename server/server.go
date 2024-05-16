4d82cf2de1e8b300a1a97f290305ed4f89730180
Thomas Mendez <30785702+thomasmendez@users.noreply.github.com>
2024-05-16 10:33:51 -0500

    Initial commit

---
+++ server/server.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"net/http"
+	"os"
+	"syscall"
+
+	"time"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type Server struct {
+	Log      *zap.Logger
+	Router   *gin.Engine
+	Shutdown chan os.Signal
+}
+
+type HealthCheck struct {
+	Status string `json:"status,omitempty"`
+	Host   string `json:"host,omitempty"`
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.Router.ServeHTTP(w, r)
+}
+
+func NewServer(router *gin.Engine, log *zap.Logger, shutdown chan os.Signal) *Server {
+	router.Use(ginzap.RecoveryWithZap(log, true))
+	router.Use(ginzap.Ginzap(log, time.RFC3339, true))
+	return &Server{
+		Router:   router,
+		Log:      log,
+		Shutdown: shutdown,
+	}
+}
+
+func (s *Server) Run(serverAddress string) {
+	s.Router.Run(serverAddress)
+}
+
+func (s *Server) SignalShutdown() {
+	s.Shutdown <- syscall.SIGTERM
+}
+
+func (s *Server) healthCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		host, err := os.Hostname()
+		if err != nil {
+			host = "unavailable"
+		}
+		data := HealthCheck{
+			Status: "up",
+			Host:   host,
+		}
+		c.JSON(http.StatusOK, data)
+	}
+}
+
+func (s *Server) SetRoutes() {
+
+	route := s.Router.Group("/api")
+
+	v1 := route.Group("/v1")
+	{
+		v1.GET("/health-check", s.healthCheck())
+	}
+
+	route.Static("/swagger", "./swagger")
+
+}
